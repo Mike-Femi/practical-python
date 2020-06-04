b00df3191dd4ae556412f247926be08c19317e8f
Mike-Femi <66252951+Mike-Femi@users.noreply.github.com>
2020-06-04 19:12:06 +0100

    Exercises on Numbers

--- Work/Ex1_8ExtraPayment_Morgage.py
+++ Work/Ex1_8ExtraPayment_Morgage.py
@@ -6,17 +6,16 @@ principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
-month = 0
+month = 0 # introduced new variable 'month'
 while principal > 0:
     month = month + 1
-# Conditioning extra $1000 payment for 4 years after year 5
-    if month >= 60 and month <= 108:
+    # Conditioning extra $1000 for the first 12 months
+    if month <= 12:
         payment = 3684.11
     else:
         payment = 2684.11
     principal = principal * (1+rate/12) - payment
     total_paid = total_paid + payment
-    print(month, round(total_paid, 2), round(principal, 2))
 
-print('Total paid =',round(total_paid, 5))
+print('Total paid =',round(total_paid, 8))
 print('Months =', month)
--- Work/Ex1_9ExtraPayment_Calculator.py
+++ Work/Ex1_9ExtraPayment_Calculator.py
@@ -1,6 +1,8 @@
-# mortgage.py
-#
-# Exercise 1.7
+# Exercise 1.9: Making an Extra Payment Calculator.
+# How much will Dave pay if he pays an extra $1000/month for 4 years
+# starting in year 5 of the mortgage?
+
+
 
 principal = 500000.0
 rate = 0.05
@@ -9,14 +11,13 @@ total_paid = 0.0
 month = 0
 while principal > 0:
     month = month + 1
-# Conditioning extra $1000 payment for 4 years after year 5
+    # Conditioning extra $1000 for 4 years after year 5
     if month >= 60 and month <= 108:
         payment = 3684.11
     else:
         payment = 2684.11
     principal = principal * (1+rate/12) - payment
     total_paid = total_paid + payment
-    print(month, round(total_paid, 2), round(principal, 2))
 
-print('Total paid =',round(total_paid, 5))
+print('Total paid =',round(total_paid, 8))
 print('Months =', month)
