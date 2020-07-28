50585f05a50b28945e145419d9d7115a46fcad80
Mike-Femi <66252951+Mike-Femi@users.noreply.github.com>
2020-07-28 22:58:40 +0100

    added property to shares() and cost() along with __slots__

--- Work/stock.py
+++ Work/stock.py
@@ -2,14 +2,24 @@ class Stock:
     '''
     An instance of a stock holding consisting of name, shares and price
     '''
+    __slots__ = ('name','_shares','price')
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
         self.price = price
+    @property
+    def shares(self):
+        return self._shares
+
+    @shares.setter
+    def shares(self, value):
+        if not isinstance(value, int):
+            raise TypeError('Expected int')
+        self._shares = value
 
     def __repr__(self):
         return f'Stock({self.name!r}, {self.shares!r}, {self.price!r})'
-
+    @property
     def cost(self):
         '''
         Return cost by : shares x price
